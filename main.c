cb65e13375ad1679ea5d021907b37e6377191d2b
pcpl2 <kontakt@patryk-lawicki.pl>
2019-02-11 23:16:02 +0100

    Implemented value collection during processor interrupts. Some minor optimizations were made.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 #include "main.h"
@@ -8,8 +8,14 @@
 #include "lcdpcf8574/lcdpcf8574.h"
 #include "pcf8574/pcf8574.h"
 
-#define INPUT_DATA			PC0
-#define	r1					216
+volatile uint16_t ADCValue = 0;
+volatile uint16_t Calculated = 0;
+volatile uint16_t PrecentFuelTank = 0;
+
+ISR(ADC_vect)//obsługa przerwania po zakończeniu konwersji ADC
+{
+	ADCValue = ADC;
+}
 
 int main( void )
 {
@@ -24,6 +30,8 @@ int main( void )
 
 	initOhms();
 
+	sei(); //Globalne uruchomienie przerwań
+
 	_delay_ms(500);
 	lcd_clrscr();
 
@@ -35,75 +43,63 @@ int main( void )
 
 void initOhms(void)
 {
-	ADMUX |= _BV(REFS0);
+	ADCSRA |= _BV(ADEN) //uruchomienie ADC
+			|_BV(ADFR) //włączenie trybu Free run
+            |_BV(ADIE) //uruchomienie zgłaszania przerwań
+            |_BV(ADSC) //rozpoczącie konwersji
+			| _BV(ADPS2); //ADPS2: (ustawienie preskalera) preskaler= 16
 
-	ADMUX |= _BV(MUX1)|_BV(MUX0);
-	ADCSRA |= _BV(ADEN);
-	ADCSRA |= _BV(ADPS0);
-	ADCSRA |= _BV(ADPS1);
-	ADCSRA |= _BV(ADPS2);
+	ADMUX |= _BV(REFS0) //VCC jako napięcie referencyjne
+			| ADC3; //wybór kanału pomiarowego na pinie ADC3
+
+	DDRC &=~ _BV(ADCINDATA); //Ustawienie Wejścia ADC
 }
 
 
 void loop(void)
 {
-  printPrecent(getOhms());
-  _delay_ms(200);
+	_Bool printNewData = calculateOhms();
+	if(printNewData)
+	{
+		printPrecent(Calculated);
+	}
 }
 
-float calculatePrecent(float ohms)
+void printPrecent()
 {
- return (ohms / 171.5) * 100;
-}
-
-void printPrecent(float ohms)
-{
-  int precent = (int) calculatePrecent(ohms);
   lcd_gotoxy(0, 0);
-  buildProgressbar(precent);
+  buildProgressbar();
+
   lcd_gotoxy(0, 1);
-  char ohmsMsg[12];
-  if((int)ohms < 100) {
-	  sprintf(ohmsMsg, "R: = %d ", (int)ohms);
-  }
-  else
-  {
-	  sprintf(ohmsMsg, "R: = %d", (int)ohms);
-  }
+  char ohmsMsg[16];
+  sprintf(ohmsMsg, "%d%% R: = %d", PrecentFuelTank, Calculated);
   lcd_puts(ohmsMsg);
-  lcd_gotoxy(12, 1);
-  char precentMsg[6];
-  if(precent < 100)
-  {
-	  sprintf(precentMsg, " %d%%", precent);
-  }
-  else
-  {
-	  sprintf(precentMsg, "%d%%", precent);
-  }
-  lcd_puts(precentMsg);
 }
 
-void buildProgressbar(int precent)
+void buildProgressbar()
 {
   const size_t PROGRESS_BAR_LENGTH = 16;
-  int progressCount = ((precent * 14) / 100);
+  uint8_t progressCount = ((PrecentFuelTank * 14) / 100);
   char progress[PROGRESS_BAR_LENGTH+1];
   progress[0] = '[';
-  for (int i = 1; i < PROGRESS_BAR_LENGTH - 1; ++i)
+  for (uint8_t i = 1; i < PROGRESS_BAR_LENGTH - 1; ++i)
   {
-      progress[i] = (i < progressCount) ? '#' : ' ';
+	  progress[i] = (i < progressCount) ? '#' : ' ';
   }
   progress[PROGRESS_BAR_LENGTH - 1] = ']';
 
   lcd_puts(progress);
 }
 
-float getOhms(void)
+_Bool calculateOhms(void)
 {
-    ADCSRA |= _BV(ADSC);
-    while(ADCSRA & _BV(ADSC)) {}
-	float uWyj =(float)(ADCL | (ADCH<<8))/1024*5;
-	float r = r1 * ( 5.0f/uWyj - 1);
-	return r;
+	uint16_t ohms = R1 * ( VREF / ( (float)(ADCValue) / 1024 * VREF) - 1);
+	if (ohms != Calculated)
+	{
+		Calculated = ohms;
+		float precent = (ohms / FullFuelTankOhms) * 100;
+		PrecentFuelTank = FLOAT_TO_INT(precent);
+		return 1;
+	}
+	return 0;
 }
--- main.h
+++ main.h
@@ -8,12 +8,25 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#define FLOAT_TO_INT(x) ((x)>=0?(int)((x)+0.5):(int)((x)-0.5))
+
+#define	R1 216
+#define VREF 5
+#define ADCINDATA PC3   //definicja ADCINDATA (wejście ADC)
+
+#define ADC5 _BV(MUX2)|_BV(MUX0)
+#define ADC4 _BV(MUX2)
+#define ADC3 _BV(MUX1)|_BV(MUX0)
+#define ADC2 _BV(MUX1)
+#define ADC1 _BV(MUX0)
+
+#define FullFuelTankOhms 171.5
 
 void loop(void);
 void initOhms(void);
-void printPrecent(float ohms);
-void buildProgressbar(int precent);
-float getOhms(void);
+void printPrecent();
+void buildProgressbar();
+_Bool calculateOhms(void);
 
 
 #endif /* MAIN_H_ */
